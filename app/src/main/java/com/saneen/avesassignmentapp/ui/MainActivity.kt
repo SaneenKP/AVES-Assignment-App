fc2cc29fee742977a4fd11d47a6c18060871bd63
Saneen K P <saneenkp2000@gmail.com>
2022-12-11 16:10:51 +0530

    retrofit implemented in main activity

--- app/src/main/java/com/saneen/avesassignmentapp/ui/MainActivity.kt
+++ app/src/main/java/com/saneen/avesassignmentapp/ui/MainActivity.kt
@@ -2,11 +2,47 @@ package com.saneen.avesassignmentapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.saneen.avesassignmentapp.R
+import com.saneen.avesassignmentapp.utils.Constants
+import com.saneen.avesassignmentapp.viewmodels.HomeViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel : HomeViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    private fun init(){
+       viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+       viewModel.getData()
+
+       observeData()
+    }
+
+    private fun observeData(){
+
+        viewModel.dataResponse.observe(this) {
+            when (it.status) {
+
+                Constants.Status.ERROR -> {
+                    Log.d("saneen", "error = ${it.message}")
+                }
+
+                Constants.Status.SUCCESS -> {
+                    Log.d("saneen", it.data.toString())
+                }
+
+                Constants.Status.LOADING -> {
+                    Log.d("saneen", "loading")
+                }
+            }
+        }
     }
 }
